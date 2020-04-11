4b92c55472cc58d38b41f9fc8fc6f5405062d079
Kyle Dunn <kdunn.code@gmail.com>
2020-04-11 19:56:20 -0400

    made first submission to kaggle

--- Titanic.py
+++ Titanic.py
@@ -5,9 +5,10 @@ from sklearn import linear_model
 #Read in the training dataset
 training  = pd.read_csv("train.csv")
 
+#Clean and derive new data
 training["Fare"] = training["Fare"].fillna(training["Fare"].dropna().median())
 training["Age"] = training["Age"].fillna(training["Age"].dropna().median())
-training["Deck"] = training["Cabin"].str[:1]
+training["Family"] = training["SibSp"]+training["Parch"]
 
 training.loc[training["Sex"] == "male", "Sex"] = 0
 training.loc[training["Sex"] == "female", "Sex"] = 1
@@ -15,12 +16,37 @@ training.loc[training["Sex"] == "female", "Sex"] = 1
 training.loc[training["Age"] < 18, "IsChild"] = 1
 training.loc[training["Age"] >= 18, "IsChild"] = 0
 
+#Set targets and features
 target = training["Survived"].values
-features = training[["Pclass", "Age", "Sex", "SibSp", "Parch", "IsChild"]].values
+features = training[["Pclass", "Age", "Sex", "SibSp", "Parch", "IsChild", "Fare"]].values
 
+#set classifier
 classifier = linear_model.LogisticRegression()
 classifier_ = classifier.fit(features, target)
 
+#Score model
 print(classifier_.score(features,target))
 
-print(training["Deck"].value_counts())
+#Read in the training dataset
+test  = pd.read_csv("test.csv")
+
+ids = test["PassengerId"]
+ids = ids.to_frame()
+
+test["Fare"] = test["Fare"].fillna(test["Fare"].dropna().median())
+test["Age"] = test["Age"].fillna(test["Age"].dropna().median())
+test["Family"] = test["SibSp"]+test["Parch"]
+
+test.loc[test["Sex"] == "male", "Sex"] = 0
+test.loc[test["Sex"] == "female", "Sex"] = 1
+
+test.loc[test["Age"] < 18, "IsChild"] = 1
+test.loc[test["Age"] >= 18, "IsChild"] = 0
+
+test = test.drop(columns=["Name", "Ticket", "Cabin", "Embarked", "PassengerId", "Family"])
+
+results = classifier.predict(test)
+
+ids.insert(1, "Survived", results, True)
+
+ids.to_csv('predictions.csv', index=False)
--- Visualize.py
+++ Visualize.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 #Read in the training dataset
 training  = pd.read_csv("train.csv")
 
+training["Family"] = training["SibSp"]+training["Parch"]
+
 fig = plt.figure(figsize=(18,6))
 
 plt.subplot2grid((2,3), (0,0))
@@ -18,5 +20,9 @@ plt.title("Survival and Age")
 plt.subplot2grid((2,3), (0,2))
 training.Pclass.value_counts(normalize=True).plot(kind="bar", alpha=0.5)
 plt.title("Survival and Class")
+
+plt.subplot2grid((2,3), (1,0))
+plt.scatter(training.Survived, training.Family, alpha=0.1)
+plt.title("Survival and Family Size")
 
 plt.show()
